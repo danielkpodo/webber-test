978385ffa2aa8122ec8b2c183cd922234686f29e
danielkpodo <kpododanielnarh@gmail.com>
2020-08-05 07:10:12 +0000

    Updates

---
+++ precache-manifest.d273d6d92e6a5cc2ab9144246103ee76.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d78d5b3f5add2d3c1601aeabdd364fd",
+    "url": "/webber-test/index.html"
+  },
+  {
+    "revision": "a39cc7a244b11d12a37c",
+    "url": "/webber-test/static/css/2.260b3d15.chunk.css"
+  },
+  {
+    "revision": "525fb67ee6abc435619e",
+    "url": "/webber-test/static/css/main.5dc44daf.chunk.css"
+  },
+  {
+    "revision": "a39cc7a244b11d12a37c",
+    "url": "/webber-test/static/js/2.24291e87.chunk.js"
+  },
+  {
+    "revision": "ab2c25260e291b20436a40ef85f6b4e3",
+    "url": "/webber-test/static/js/2.24291e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525fb67ee6abc435619e",
+    "url": "/webber-test/static/js/main.51ac8c0d.chunk.js"
+  },
+  {
+    "revision": "ecaaddef7f5871ca0d12",
+    "url": "/webber-test/static/js/runtime-main.5e745cc7.js"
+  },
+  {
+    "revision": "e34d26d866e228e77f8ef6ec0f66250c",
+    "url": "/webber-test/static/media/Menu Icon.e34d26d8.svg"
+  },
+  {
+    "revision": "327c71bc9211b67882dbc274f733b993",
+    "url": "/webber-test/static/media/activity.327c71bc.svg"
+  },
+  {
+    "revision": "5e63c1327d5f5629334579c5e5900fe9",
+    "url": "/webber-test/static/media/new.5e63c132.svg"
+  },
+  {
+    "revision": "0dacf0be33eb6f3c1598e8feb8aae905",
+    "url": "/webber-test/static/media/profit.0dacf0be.svg"
+  },
+  {
+    "revision": "63f9d1233779f949c9fe540ac14d291d",
+    "url": "/webber-test/static/media/yuna.63f9d123.jpg"
+  }
+]);
